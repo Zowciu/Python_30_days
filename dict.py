9601d2fc65e39ccb340189eb0507b8b6ec8eaddd
Szymon Zowczak <szymon.zowczak@gmail.com>
2026-04-08 19:35:53 +0200

    dictionaries tutorial tasks

---
+++ dict.py
@@ -0,0 +1,36 @@
+
+dog = {
+    "name":"maciek",
+    "bread":"husky",
+    "legs":4,
+    "age":10
+}
+values = dog.values()
+print(values)
+
+
+students_dict = {
+    "first_name": "Szymon", 
+    "last_name":"Zowczak", 
+    "gender":"male", 
+    "age":27, 
+    "marital status":"middle", 
+    "skills":["Python","C++"],  
+    "country":"Poland", 
+    "city":"Poznań", 
+    "address":"Kasprowicza 12"
+}
+
+print(len(students_dict))
+
+print(students_dict.get("skills"))
+
+students_dict["skills"].append("C")
+
+print(students_dict.get("skills"))
+
+print(students_dict.values())
+print(students_dict.keys())
+
+list_of_tuples = list(students_dict.items())
+print(list_of_tuples)
